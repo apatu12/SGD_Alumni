a794af079de24f69ed9d2b6156df10aecc854d3e
Patricio <jonioaraujolekipatricio@gmail.com>
2025-06-12 15:23:54 +0900

    Update project no Aumenta  SSL Aiven

--- Si_Alumni/settings.py
+++ Si_Alumni/settings.py
@@ -96,12 +96,27 @@ WSGI_APPLICATION = 'Si_Alumni.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'ca': os.path.join(BASE_DIR, 'ca.pem'),
+        },
     }
 }
+
 # DATABASES = {
 #     'default':{
 #         'ENGINE': 'django.db.backends.mysql',
